d6f1881738237893d2834441768e97c69283eeb1
Ignacio Avilés <88689569+avilesxd@users.noreply.github.com>
2022-09-06 23:18:35 -0300

    Add files via upload

---
+++ src/Operadores.java
@@ -0,0 +1,36 @@
+public class Operadores {
+    public int suma(int num, int num2){
+        int resultado = 0;
+        resultado = num + num2;
+        return resultado;
+    }
+    public int resta(int num, int num2){
+        int resultado = 0;
+        resultado = num - num2;
+        return resultado;
+    }
+    public int multiplicacion(int num, int num2){
+        int resultado = 0;
+        resultado = num * num2;
+        return resultado;
+    }
+    public int division(int num, int num2){
+        int resultado = 0;
+        resultado = num / num2;
+        return resultado;
+    }
+    /* public int NuemeroElevado(int num, int num2){
+        double num11;
+        System.out.println("Ingrese el número al cual desea sacar su raíz cuadrada");
+        num11 = sc.nextInt();
+        double resultado = Math.sqrt(num11);
+        System.out.println("La raíz cuadrada de " + num11 + " es " + resultado + "\n");
+    }
+    public int RaizCuadrada(int num, int num2){
+        double num11;
+        System.out.println("Ingrese el número al cual desea sacar su raíz cuadrada");
+        num11 = sc.nextInt();
+        double resultado = Math.sqrt(num11);
+        System.out.println("La raíz cuadrada de " + num11 + " es " + resultado + "\n");
+    } */
+}
